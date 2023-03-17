776b3ac0a1dce3c0c6708c6af8f0845bd9eb24bd
GdMess01 <god10.mess@gmail.com>
2023-03-17 10:00:12 -0700

    10-numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main-print the alphabet in lowercase
+* 
+* Description:using the main function
+* this program prints "the alphabet in lowercase, followed by a new line
+* Return:0
+*/
+
+int main(void) {
+    int i;
+
+    for (i = 0; i <= 9; i++) 
+    {
+        printf("%d", i);
+    }
+    putchar('\n');
+    return (0);
+}
