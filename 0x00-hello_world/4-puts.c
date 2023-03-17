fccb42c12139a69e04b2bcaceb64cf2877c2dc8c
GdMess01 <god10.mess@gmail.com>
2023-03-16 17:30:21 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0
- */
+* main - Entry point
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        Return (0);
+        return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 /**
-* You should produce the exacte same output as in the example
-* Warnings are allowed
-* 
-* Your program should return 0
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-       char c;
-       int i;
-       long li;
-       long long lli;
-       float f;
+        char c;
+        int i;
+        long li;
+        long long lli;
+        float f;
 
-       printf("size of a char: %ld byte(s)\n", sizeof(c));
-       printf("size of int: %ld byte(s)\n", sizeof(i));
-       printf("size of a long: %ld byte(s)\n", sizeof(li));
-       printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-       printf("size of a float: %ld byte(s)\n", sizeof(f));
-       return (0);
+        printf("size of a char: %ld byte(s)\n", sizeof(c));
+        printf("size of a int: %ld byte(s)\n", sizeof(i));
+        printf("size of a long: %ld byte(s)\n", sizeof(li));
+        printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+        printf("size of a float: %ld byte(s)\n", sizeof(f));
+        return (0);
 }
