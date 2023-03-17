468f50b2c55fc916025e0ef0deaaa074c9dd3ff6
GdMess01 <god10.mess@gmail.com>
2023-03-17 15:35:04 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main() {
     int j;
     for (i = 48; i <= 57; i++){
         for (j = 48; j <= 57; j++){
-            if (i < j ){
+            for (k < j ){
                 putchar(i);
                 putchar(j);
                 if (i != 56 || j != 57) {
                     putchar(',');
-                    putchar('  ');
+                    putchar(' ');
                 }
             }
         }
