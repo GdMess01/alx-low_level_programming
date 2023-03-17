ea29a4cedaa0908f6880673594bf633f2fd76e59
GdMess01 <god10.mess@gmail.com>
2023-03-17 15:38:54 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,20 @@
  * this program prints "all possible different combinations of two digits
  * Return: 0
  */
-int main() {
+int main()
+{
     int i;
     int j;
-    for (i = 48; i <= 57; i++){
-        for (j = 48; j <= 57; j++){
-            for (k < j ){
+    for (i = 48; i <= 57; i++)
+    {
+        for (j = 48; j <= 57; j++)
+        {
+            if (i < j )
+            {
                 putchar(i);
                 putchar(j);
-                if (i != 56 || j != 57) {
+                if (i != 56 || j != 57) 
+                {
                     putchar(',');
                     putchar(' ');
                 }
