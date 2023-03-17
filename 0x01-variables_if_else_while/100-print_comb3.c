42ff99209495ea76eb47f0eb6127554a6a3c44cb
GdMess01 <god10.mess@gmail.com>
2023-03-17 15:09:55 -0700

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,13 @@
-
 #include <stdio.h>
+
 /**
-* main - print the combination of brains and materials
-* 
-* Description: using the main function
-* this program prints "all possible different combinations of two digits
-* Return: 0
-*/
+ * main - print the combination of brains and materials
+ * 
+ * Description: using the main function
+ * this program prints "all possible different combinations of two digits
+ * Return: 0
+ */
 int main() {
-    // Write C code here
     int i;
     int j;
     for (i = 48; i <= 57; i++){
@@ -16,8 +15,10 @@ int main() {
             if (i < j ){
                 putchar(i);
                 putchar(j);
-                putchar(',');
-                putchar(' ');
+                if (i != 56 || j != 57) {
+                    putchar(',');
+                    putchar(' ');
+                }
             }
         }
     }
