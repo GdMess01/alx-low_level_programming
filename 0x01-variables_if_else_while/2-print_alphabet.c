dd8105e74c213baf25a333f1546ce6dc9d21e661
GdMess01 <god10.mess@gmail.com>
2023-03-17 09:04:32 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+* main - print the alphabet in lowercase
+* 
+* Description: using the main function
+* this program prints "the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+
+int main() {
+    char str[] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++) 
+    {
+        putchar(str[i]);
+        putchar('\n');
+    }
+
+    return (0);
+}
