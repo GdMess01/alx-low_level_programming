fc85878549c0b1cddc779d3198b65b73b007489a
GdMess01 <god10.mess@gmail.com>
2023-03-21 13:04:24 -0700

    abs_value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - add absolute value of numbers
+ * @c: The character to be checked
+ * Return: -a if the number is negative and a if is positive
+ */
+
+int _abs(int a)
+{
+    if (a < 0)
+    {
+    int abs_val;
+    abs_val = a * -1;
+    return(abs_val);
+    }
+    return(a);
+}
