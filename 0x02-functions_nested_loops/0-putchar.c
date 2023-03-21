cd0b545064bcfeb8e3c0f70a428e399f3b77d61d
GdMess01 <god10.mess@gmail.com>
2023-03-21 04:45:57 -0700

    first for today

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 int main(void)
 {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-char initial[] = "_putchar";
-printf("%s\n", initial);
-return (0);
+    return 0;
 }
