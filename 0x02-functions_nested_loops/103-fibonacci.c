de1b4856f1605145a7d011a79f65e36f0ddecfeb
GdMess01 <god10.mess@gmail.com>
2023-03-22 10:15:32 -0700

    even_fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,26 +6,21 @@
  */
 int main(void) 
 {
-    int i = 0;
+    int i = 0, sum = 2;
     long f, f1 = 2, f2 = 1;
-    while (i < 32)
+    while (f < 4000000)
     {
-        if (i == 0)
-        printf("%ld", f2);
-        else if (i == 1)
-        printf(", %ld", f1);
-        else
+        f = f1 + f2;
+        f2 = f1;
+        f1 = f;
+        if (f % 2 == 0 )
         {
-            f = f1 + f2;
-            f2 = f1;
-            f1 = f;
-            if (f % 2 == 0)
-            {
-                printf(", %ld", f);
-            }
+            sum += f;
+                
         }
         i++;
     }
+    printf("%ld", sum);
     printf("\n");
     
 return 0;
