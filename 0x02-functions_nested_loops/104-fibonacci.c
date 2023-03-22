af5f6f63310f0694baf21b3ee89fbaf9c8bba396
GdMess01 <god10.mess@gmail.com>
2023-03-22 10:30:02 -0700

    100_fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 100 numbers of fibonacci numbers
+ * Return: Always success
+ */
+int main(void)
+{
+    int i = 0;
+    long long f, f1 = 2, f2 = 1;
+    while (i < 100)
+    {
+        if (i == 0)
+        printf("%lld", f2);
+        else if (i == 1)
+        printf(", %lld", f1);
+        else
+        {
+            f = f1 + f2;
+            f2 = f1;
+            f1 = f;
+            printf(", %lld", f);
+        }
+        i++;
+    }
+    printf("\n");
+
+return 0;
+}
