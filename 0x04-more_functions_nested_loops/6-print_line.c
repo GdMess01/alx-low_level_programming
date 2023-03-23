df919d2b625fb4012c796c61e61377db4f8534e9
GdMess01 <god10.mess@gmail.com>
2023-03-23 09:17:55 -0700

    draw_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_line - prints a line
+* Return: nothing
+*/
+void print_line(int n)
+{
+int i;    
+for (i = 0; i <= n; i++)
+{
+putchar(95);
+}
+putchar('\n');
+}
