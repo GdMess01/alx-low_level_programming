f61793f0f61b42fdbcc6be5f17f6bdddc7e10311
GdMess01 <god10.mess@gmail.com>
2023-03-23 11:11:39 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - prints square from '#'
+* Return: nothing
+*/
+
+void print_square(int size)
+{
+if (size > 0)
+{
+int i, j;	
+for (i = 0; i <= size; i++)
+{
+for (j = 0; j <= size; j++)
+{
+putchar('#');
+}
+putchar('#');
+putchar('\n');
+}
+}
+else
+{
+putchar('\n');
+}
+}
