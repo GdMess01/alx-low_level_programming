3029978aaa18a04eeb6aac95dbc900dfff462d5f
GdMess01 <god10.mess@gmail.com>
2023-03-23 06:39:48 -0700

    digit_or_not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - checks if the input is a digit
+* @c: intger to be used
+* Return: 1 if is digit and 0 if is not
+*/
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+return (0);
+}
