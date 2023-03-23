a5fb4b6b010b3d26bc504f1f84f2a48629f7b72d
GdMess01 <god10.mess@gmail.com>
2023-03-23 16:52:59 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints a triangle
+* Return: nothing
+*/
+void print_triangle(int size)
+{
+int i, j;
+if (size > 0)
+{
+        for (i = 1; i <= size; i++)
+        {
+               for((j = size - i); j > 0; j--)
+               {
+                      putchar(' ');
+               }
+               for (j = 0; j < i; j++)
+               {
+                      putchar('#');
+               }
+               if (i == size)
+               {
+                      continue;
+               }
+               putchar('\n');
+        }
+}
+}
