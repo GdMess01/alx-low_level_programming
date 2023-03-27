8e7563d24dcb35a9b89ef42f14d3518c2c1a3364
GdMess01 <god10.mess@gmail.com>
2023-03-27 13:51:15 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * reset_to_98 - reset the value to 98
+ * reset_to_98 - reset to 98
  * Return: nothing
  */
 
-void reset_to_98(int *m)
+void reset_to_98(int *n)
 {
-	*m = 98;
+    *n = 98;
 }
