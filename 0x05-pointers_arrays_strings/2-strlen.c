192e286f6bc2e9a4ce98e9a0d0751ef64524c53f
GdMess01 <god10.mess@gmail.com>
2023-03-28 05:18:20 -0700

    len_string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * strlen - determine the length of a string
+ * @s: te be added
+ * Return: an integer
+ */
+ 
+int _strlen(char *s)
+{
+    return(strlen(s));
+}
