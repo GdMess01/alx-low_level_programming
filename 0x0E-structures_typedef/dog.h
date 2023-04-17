7a0d1ad86e69d53fe239a45fadff2546bf44d2ca
GdMess01 <god10.mess@gmail.com>
2023-04-17 07:37:42 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct my_struct - Short description
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};	
